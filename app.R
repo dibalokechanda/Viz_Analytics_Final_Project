7006aba9cf42c52c9d234b566a75af8fbe45a89c
dibalokechanda <dibaloke.chanda@marquette.edu>
2024-05-03 15:06:55 -0500

    Tabs are finalized

--- app.R
+++ app.R
@@ -1,30 +1,47 @@
+# Load the Libraries
 library(shiny)
 library(shinydashboard)
+library(shinydashboardPlus)
+
 
 sidebar <- dashboardSidebar(
+  width = 350,
   sidebarMenu(
+    menuItem("Airbnb Listings", tabName = "listing"),
     menuItem("Crime Rate", tabName = "crime"),
-    menuItem("Feedback", tabName = "feedback")
+    menuItem("Feedback", tabName = "feedback"),
+    menuItem("About", tabName = "about")
   )
 )
 
 body <- dashboardBody(
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
+  ),
   tabItems(
+    tabItem(tabName = "listing",
+            h2("Listing Information and Crime Density")
+    ),
     tabItem(tabName = "crime",
             h2("Visualize Crime Rate by Category")
     ),
-    
     tabItem(tabName = "feedback",
             h2("Provide Feedback for a Listing")
+    ),
+    tabItem(tabName = "about",
+            h2("About")
     )
   )
 )
 
 shinyApp(
   ui = dashboardPage(
-    dashboardHeader(title = "Safe Airbnb"),
-    sidebar,
-    body,
+    header =dashboardHeader(title = "Safe Airbnb"),
+    sidebar=sidebar,
+    body=body,
+    skin = "red",
+    controlbar = dashboardControlbar(collapsed = FALSE, skinSelector()),
+
   ),
   server = function(input, output) {
     
