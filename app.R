f55a6b3253dfa64d854b7aeb5a21335c00ff104f
dibalokechanda <dibaloke.chanda@marquette.edu>
2024-05-03 14:07:04 -0500

    Tabs added

--- app.R
+++ app.R
@@ -1,1 +1,33 @@
-# Code
+library(shiny)
+library(shinydashboard)
+
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Crime Rate", tabName = "crime"),
+    menuItem("Feedback", tabName = "feedback")
+  )
+)
+
+body <- dashboardBody(
+  tabItems(
+    tabItem(tabName = "crime",
+            h2("Visualize Crime Rate by Category")
+    ),
+    
+    tabItem(tabName = "feedback",
+            h2("Provide Feedback for a Listing")
+    )
+  )
+)
+
+shinyApp(
+  ui = dashboardPage(
+    dashboardHeader(title = "Safe Airbnb"),
+    sidebar,
+    body,
+  ),
+  server = function(input, output) {
+    
+    
+  }
+)
